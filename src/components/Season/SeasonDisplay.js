3cb7ac5a17a48ff561539d2016406025dcecd96b
rinsarmu <roberaensermu@gmail.com>
2023-03-05 19:39:06 +0300

    adding styling

--- src/components/Season/SeasonDisplay.js
+++ src/components/Season/SeasonDisplay.js
@@ -1,18 +1,41 @@
 import React from 'react'
 
+const seasonConfig= {
+    summer:{
+        text: 'Lets hit the beach',
+        iconName: 'sun'
+
+    },
+    winter: {
+        text: 'Burr, it is cold',
+        iconName: 'snowflake'
+
+    }
+
+}
+
 const getSeason =(lat, month)=>{
     if(month > 2 && month < 9){
-        return lat > 0 ? 'Summer' : 'Winter'
+        return lat > 0 ? 'summer' : 'winter'
     } else {
-        return lat > 0? 'Winter' : 'Summer'
+        return lat > 0? 'winter' : 'summer'
     }
 }
 
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth())
+    const {text, iconName} = seasonConfig[season]
+    //   const text = season === 'Winter'? 'Burr, It is chill' : "Let's hit the beach"
+
+    //   const icon = season === 'Winter'? 'snowflake' : 'sun'
   return (
-    <div>
-        {season}
+    <div className='card'>
+        <div className='card-content'>
+        <i className={`${iconName} icon`}></i>
+        <h1>{text}</h1>
+        <i className={`${iconName} icon`}></i>
+        </div>
+       
     </div>
   )
 }
