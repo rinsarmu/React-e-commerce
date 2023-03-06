f371a49cde787deae1c569b429c7cedac930022e
rinsarmu <roberaensermu@gmail.com>
2023-03-06 15:55:55 +0300

    building list with redux

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -3,11 +3,26 @@ import { connect } from 'react-redux'
 
 
 class SongList extends React.Component {
+    renderList=()=>{
+        return this.props.songs.map(_song =>{
+            return(
+                <div className='item' key={_song.title}>
+                    <div className='right floated content'>
+                        <button className='ui button primary'> Select</button>
+                    </div>
+                    <div className='content'>
+                      <h3>{_song.title}</h3>  
+
+                    </div>
+                </div>
+            )
+        }) 
+    }
     render(){
         console.log(this.props)
         return(
-            <div>
-                Song lIst
+            <div className='ui divided list'>
+               {this.renderList()}
             </div>
         )
     }
