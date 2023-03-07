094bb71c1c3e022278ea05627dcf538a5f1caec1
rinsarmu <roberaensermu@gmail.com>
2023-03-07 12:34:08 +0300

    list building

--- Blog/src/actions/index.js
+++ Blog/src/actions/index.js
@@ -2,7 +2,7 @@
 import axios from '../axios'
 export const fetchPosts =()=>async (dispatch, getState)=>{
         const promise = await axios.get('/posts')
-         dispatch({type: 'FETCH_POSTS', payload: promise})
+         dispatch({type: 'FETCH_POSTS', payload: promise.data})
     }
 
 
--- Blog/src/components/PostList.js
+++ Blog/src/components/PostList.js
@@ -5,11 +5,31 @@ class PostList extends Component{
     componentDidMount(){
         this.props.fetchPosts()
     }
+
+    renderList=()=>{
+        console.log('ddd')
+        console.log(this.props)
+        return this.props.posts.map(post=>{
+            return (
+                <div className='item' key ={post.id}>
+                    <i className='large middle aligned icon user'/>
+                    <div className="content">
+                        <div className="description">
+                            <h2>{post.title}</h2>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                </div>
+            )
+        })
+
+    }
     render(){
-        console.log(this.props.posts)
+        // console.log(this.props.posts)
+        console.log(this.props.posts.data)
         return(
-         <div>
-        hello
+         <div className='ui relaxed divided list'>
+        { this.renderList()}
             </div>
         )
     }
